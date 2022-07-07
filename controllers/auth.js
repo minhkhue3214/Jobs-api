8b163d39f1ce68f858d49c2d93d15e8403c84c6c
minh khue <dominh3214@gmail.com>
2022-07-07 18:15:03 +0700

    need to fix login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,27 +1,29 @@
 const User = require('../models/User')
 const {StatusCodes} = require('http-status-codes')
-const {BadRequestError} = require('../errors')
-const bcrypt = require('bcryptjs')
+const {BadRequestError,UnauthenticatedError} = require('../errors')
+const jwt = require('jsonwebtoken')
 
 const register = async (req,res) =>{
-    // if(!name || !email || !password){
-    //     throw new BadRequestError('Please provide name,email,password')
-    // }
-    // Check in the controllers
-
-    // const {name,email,password} = req.body
-    // console.log(req.body)
-
-    // const salt = await bcrypt.genSalt(10);
-    // const hashedPassword = await bcrypt.hash(password,salt)
-
-    const tempUser = {name,email,password:hashedPassword}
-    console.log(tempUser)
-    const user = await User.create({...tempUser})
-    res.status(StatusCodes.CREATED).json({user})
+    const user = await User.create({...req.body})
+    const token = user.createJWT()
+    res.status(StatusCodes.CREATED).json({user:{name:user.name},token})
 }
 
 const login = async (req,res) =>{
+    const {email,password} = req.body
+
+    if(!email || !password){
+        throw new BadRequestError('Please provide email and password')
+    }
+    const user = await User.findOne({email})
+    //compare password
+    if(!user){
+        throw new UnauthenticatedError('Invalid Credentials')
+    }
+
+    const token = user.createJWT();
+    res.status(StatusCodes.OK).json({user:{name:user.name},token})
+
     res.send('login user')
 }
 
